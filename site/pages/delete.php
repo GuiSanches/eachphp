d79b2ac6dbd17a6dd14ef2930d544d8b5f195d8e
MatKurianski <38268201+MatKurianski@users.noreply.github.com>
2018-05-30 15:11:58 -0300

    Fix #8 Ignore

--- site/pages/delete.php
+++ site/pages/delete.php
@@ -1,12 +1,7 @@
 <?php
-	$login = false;
 	if (!isset($_SESSION)){
 		session_start();     
-		    if (isset($_SESSION['login'])) {
-		    	$login = $_SESSION['nome'];
-		    } 
-      }
-
+     }
       	$conn = new mysqli("localhost", "root", "", "coolsunday"); //Conexão com banco de dados estabelecida.
 		$id = mysqli_real_escape_string($conn, $_REQUEST['id']);
 		$vend = mysqli_real_escape_string($conn, $_REQUEST['vend']);
