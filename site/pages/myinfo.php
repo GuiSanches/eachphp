9ac9fd27b6354e2ad5efcf4020c9af8d3d19cc4c
MatKurianski <38268201+MatKurianski@users.noreply.github.com>
2018-06-08 16:05:19 -0300

    Revert "Revert "Merge branch 'master' of https://github.com/MatKurianski/eachphp""
    This reverts commit 72a7bce25683bdca6acfc5a67e5b9efb17f367f8.

--- site/pages/myinfo.php
+++ site/pages/myinfo.php
@@ -6,12 +6,16 @@
 	
 		    if (isset($_SESSION['login'])) {
 		    	$login = $_SESSION['nome'];
+<<<<<<< HEAD
 		    }else{
 		    	echo"<script type='text/javascript'>
 		    	window.open('/eachphp/index.php', '_parent'); 
        </script>
     	    	";
 		    }
+=======
+		    } 
+>>>>>>> 3449cdfc61af783e32dcbe4c4ec4eb9ed307505b
       }
  ?>
 <head>
@@ -19,7 +23,11 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css">
+<<<<<<< HEAD
  <link rel="stylesheet" type="text/css" href="css/myinfo.css">
+=======
+ <link rel="stylesheet" type="text/css" href="css/productsby.css">
+>>>>>>> 3449cdfc61af783e32dcbe4c4ec4eb9ed307505b
  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js"></script>
@@ -34,17 +42,35 @@
 <br>
 <?php 
 	$conn = new mysqli("localhost", "root", "", "coolsunday"); //Conexão com banco de dados estabelecida.
+<<<<<<< HEAD
 
 
 
 		echo("<h3 style=\"color: white; font-family: 'Do Hyeon', sans-serif\" align=left><span style=\"display:block; height: 30px;\">&emsp;&emsp;Seus dados</span></h3>");
 
+=======
+	$vend = mysqli_real_escape_string($conn, $_REQUEST['vend']); 
+
+	if ($login != $vend) {
+		echo("<h3 style=\"color: white; font-family: 'Do Hyeon', sans-serif\" align=left><span style=\"display:block; height: 30px;\">&emsp;&emsp;Produtos por: {$vend}</span></h3>");
+	} else {
+		echo("<h3 style=\"color: white; font-family: 'Do Hyeon', sans-serif\" align=left><span style=\"display:block; height: 30px;\">&emsp;&emsp;Meus dados</span></h3>");
+	}
+
+?>
+
+<div class="container">
+	<div class="row">
+
+		<?php
+>>>>>>> 3449cdfc61af783e32dcbe4c4ec4eb9ed307505b
 
 			$conn = new mysqli("localhost", "root", "", "coolsunday"); //Conexão com banco de dados estabelecida.
 			$sql = mysqli_query($conn,"SELECT * FROM produtos ORDER BY id DESC LIMIT 0, 1") or die (mysqli_error($conn)); //Seleciona todas as informações sobre o produto com o maior ID.
 			mysqli_set_charset($conn,"utf8"); 
 			$dados = mysqli_fetch_array($sql); //Armazena todas as linhas do produto com o maior ID.
 			$linha =  $dados['id'];
+<<<<<<< HEAD
 		
 $user = $_SESSION['login'];
 
@@ -117,3 +143,66 @@ $user = $_SESSION['login'];
 
 </body>
 </html>
+=======
+			$vend = mysqli_real_escape_string($conn, $_REQUEST['vend']);
+
+			while ($linha>0) {
+				$sql = mysqli_query($conn,"SELECT * FROM produtos WHERE id=$linha") or die (mysqli_error($conn)); //Começa as putaria.
+				$dados = mysqli_fetch_array($sql);
+
+				if ($dados != null && $dados['Aprovado'] == true && $dados['Vendedor'] == $vend && $login != $dados['Vendedor']) {
+						
+					echo("<div class=\"col-md-3 col-sm-6 p-3\">
+					<div class=\"card text-center\" > 
+						<div class=\"card-block box shadow\" >
+							<div class=\"card-title\"> 
+								<h4 class=\"card-title-text\">{$dados['Nome']}</h4>
+							</div>
+							<img src=\"produtos/{$dados['id']}.jpg\" class=\"img-fluid\" width=\"110px\">
+								
+							<div class=\"card-text\">
+								<span style=\"color: green; font-weight: bold;\">Preço: </span> R$ {$dados['Preco']}<br>
+								<span style=\"color: red; font-weight: bold;\">Vendedor: </span><a href=\"productsby.php?vend={$dados['Vendedor']}\">{$dados['Vendedor']}</a>
+								<br>
+								<form action=\"desc.php\" method=\"post\">
+									<a style=\"margin-top: 10px;\" href=\"desc.php?id={$dados['id']}\" name=\"id\" id=\"id\" class=\"btn btn-primary\">Ver mais</a>
+								</form>
+							</div>
+						</div>
+					</div>
+				</div>");
+				} else if ($dados != null && $dados['Vendedor'] == $vend && $dados['Vendedor'] == $_SESSION['nome']) {
+						
+					echo("<div class=\"col-md-3 col-sm-6 p-3\">
+					<div class=\"card text-center\" > 
+						<div class=\"card-block box shadow\" >
+							<div class=\"card-title\"> 
+								<h4 class=\"card-title-text\">{$dados['Nome']}</h4>
+							</div>
+							<img src=\"produtos/{$dados['id']}.jpg\" class=\"img-fluid\" width=\"110px\">
+								
+							<div class=\"card-text\">
+<span style=\"color: green; font-weight: bold;\">Preço: </span> R$ {$dados['Preco']}<br>
+								<form action=\"desc.php\" method=\"post\">
+									<a style=\"margin-top: 10px;\" href=\"desc.php?id={$dados['id']}\" name=\"id\" id=\"id\" class=\"btn btn-primary\">Editar</a>
+									<a style=\"margin-top: 10px;\" href=\"delete.php?id={$dados['id']}&vend={$dados['Vendedor']}\" name=\"id\" id=\"id\" class=\"btn btn-danger\">Deletar</a>
+								</form>
+							</div>
+						</div>
+					</div>
+				</div>");
+				}
+				$linha--;
+			}
+?>
+
+
+		
+	</div>
+</div>
+
+
+
+</body>
+</html>
+>>>>>>> 3449cdfc61af783e32dcbe4c4ec4eb9ed307505b
